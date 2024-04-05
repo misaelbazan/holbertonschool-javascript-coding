c894f1f3dfa97cfff4a09897a3619c2ffce22b7f
misaelbazan <misaelbfranco@gmail.com>
2024-04-05 00:56:52 +0000

    1st task file

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+// Write a script that writes a string to a file
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const strContent = process.argv[3];
+
+fs.writeFile(filePath, strContent, { encoding: 'utf8' }, err => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+});
