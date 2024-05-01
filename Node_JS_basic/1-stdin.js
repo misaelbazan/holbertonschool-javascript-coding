c182cd7ca46d015af8b937f70beae29017301d0d
misaelbazan <misaelbfranco@gmail.com>
2024-04-30 23:02:55 -0500

    1st task file

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,9 +2,8 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', (input) => {
-  const name = input.trim();
-  console.log(`Your name is: ${name}`);
+process.stdin.on('data', (name) => {
+  process.stdout.write(`Your name is: ${name}`);
   process.exit();
 });
 
