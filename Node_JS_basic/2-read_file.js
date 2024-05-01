9bcec46672c25b21a50e9d8b36e9c8c5ccc06277
misaelbazan <misaelbfranco@gmail.com>
2024-05-01 14:33:15 -0500

    2th task file

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,35 @@
+function countStudents(fileName) {
+  const fs = require('fs');
+
+  const filePath = fileName;
+
+  try {
+    // Read the csv file synchronously
+    const data = fs.readFileSync(filePath, { encoding: 'utf-8' });
+    // Split data into rows
+    const rows = data.split('\n').filter(Boolean);
+    // Parse Rows
+    const headers = rows.shift().split(',');
+    const records = [];
+
+    rows.forEach((row) => {
+      const values = row.split(',');
+      const record = {};
+      values.forEach((value, index) => {
+        record[headers[index]] = value;
+      });
+      records.push(record);
+    });
+
+    console.log('Number of students:', rows.length);
+    const studentsCs = Object.values(records).filter((value) => value.field === 'CS');
+    console.log(`Number of students in CS: ${studentsCs.length}. List: ${studentsCs.map((name) => name.firstname).join(', ')}`);
+    const studentsSwe = Object.values(records).filter((value) => value.field === 'SWE');
+
+    console.log(`Number of students: ${studentsSwe.length}. List: ${studentsSwe.map((user) => user.firstname).join(', ')}`);
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
